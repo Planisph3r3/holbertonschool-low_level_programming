897bd0f3420700a49948d7a49c452d357cb101bb
root <ennzopz@gmail.com>
2023-06-19 14:34:03 -0700

    Printing exactly "with proper grammar, but the outcome is a piece of art"

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Starting program
+ *
+ * Description: Printing a text to StdOut using printf() function
+ *
+ * Return: Zero equals success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
