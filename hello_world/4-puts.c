9d8e1fceced8e4d427c5399b926640960608ed95
root <ennzopz@gmail.com>
2023-06-19 13:36:36 -0700

    Printing on StdOut with puts()

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: Write on screen using puts()
+ *
+ * Return: Zero on success
+*/
+
+int main(void)
+{
+int word = "Programming is like building a multilingual puzzle\n";
+puts(word);
+return (0);
+}
