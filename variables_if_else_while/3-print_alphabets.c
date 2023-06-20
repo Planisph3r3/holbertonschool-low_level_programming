e75879eea00d7c50196efdd7b794c98c2d285994
root <ennzopz@gmail.com>
2023-06-20 16:45:47 -0700

    Printing the alphabet in lowercase, and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - starting program
+*
+* Description: Printing the alphabet in lowercase, and then in uppercase
+*
+* Return: Zero equals success
+*/
+int main(void)
+{
+char az[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int x;
+for (x = 0; x <= 50; x++)
+{
+putchar(az[x]);
+}
+putchar('\n');
+return (0);
+}
