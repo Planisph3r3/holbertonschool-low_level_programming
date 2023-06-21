9f6b2f041c48c70e1b86f55d21dca718e3cd80fb
root <ennzopz@gmail.com>
2023-06-21 12:38:17 -0700

    Printing and comparing the last digit of a number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,9 +11,9 @@
 int main(void)
 {
 int n;
-char m = n % 10;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+char m = n % 10;
 if (m > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, m);
@@ -22,7 +22,7 @@ else if (m == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, m);
 }
-else if (m != 0, m < 6)
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
