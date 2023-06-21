b4d81d88089c6e26c5ecf00facc55225543e99d0
root <ennzopz@gmail.com>
2023-06-21 13:55:24 -0700

    Prints all natural numbers & 0 with , in between

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@
 int main(void)
 {
 int x;
-for (x = 48; x <= 57; x++)
+for (x = 48; x < 58; x++)
 {
 putchar(x);
-}
+
 if (x != 57)
 {
 putchar(44);
 putchar(32);
+}
 }
 putchar('\n');
 return (0);
