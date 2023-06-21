52ba79e44b686459279fbe468ba8f786213ddab2
root <ennzopz@gmail.com>
2023-06-21 13:50:50 -0700

    Prints all natural numbers & 0 with , in between

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+/**
+*main - starting program
+*
+* Description: Prints all natural numbers & 0 with ',' in between
+*
+* Return: Zero equals success
+*/
+int main(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+{
+putchar(x);
+}
+if (x != 57)
+{
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
