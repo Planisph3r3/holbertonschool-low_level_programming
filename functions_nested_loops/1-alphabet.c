b3ad85219782ac93495d745084ccc69e7987f0e8
root <ennzopz@gmail.com>
2023-06-21 17:21:58 -0700

    Program that prints the whole alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * 
+ *
+ * Description: Program that prints the whole alphabet
+ *
+ * 
+ */
+	
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet
+}	
+void print_alphabet(void)
+{
+char abc;
+
+for (abc == 'a'; abc <= 'z'; abc++)
+{
+_putchar(abc)
+}
+_putchar(\n)
+}
