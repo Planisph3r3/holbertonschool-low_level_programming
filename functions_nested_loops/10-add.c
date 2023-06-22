ad8b81b862911f2f22ba213014d1b6d191f576fa
root <ennzopz@gmail.com>
2023-06-22 15:56:19 -0700

    function that adds between two ints

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition function
+ *
+ * Description: function that adds between two ints
+ *
+ * Return: C value equals success
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
