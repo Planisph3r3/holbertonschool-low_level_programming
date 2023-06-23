303a9e0c8e790a1548dfd127e6bf42cc250c92ec
root <ennzopz@gmail.com>
2023-06-23 14:53:27 -0700

    Prints from 0 to 9 excluding 2&4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,13 @@
 */
 
 void print_most_numbers(void)
-2	{
-3	    int d;
-4	        for (d = 48; d <= 57; d++)
-5	{
-6
-7	               if (d == 50 || d == 52)
-8	               continue;
-9	               putchar(d);
-10	}
-11	        putchar('\n');
-12	}
+{
+	int d;
+for (d = 48; d <= 57; d++)
+{
+if (d == 50 || d == 52)
+continue;
+_putchar(d);
+}
+_putchar('\n');
+}
