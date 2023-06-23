1d9ec439628c3807bfa3f3e3bca36c112842fce6
root <ennzopz@gmail.com>
2023-06-23 16:14:38 -0700

    Prints certain length of lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - Prints certain length of lines
+ *
+ * @n: value of length
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int m;
+for (m = 0; (m < n); m++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+
