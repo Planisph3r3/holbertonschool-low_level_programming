a8e9d353ae9de21056d11204ab81e482762da599
root <ennzopz@gmail.com>
2023-06-24 09:48:29 -0700

    Prints numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: Void
+ */
+void print_numbers(void)
+{
+int d;
+for (d = 48; d <= 57; d++)
+{
+_putchar(d);	
+}
+_putchar('\n');
+}
