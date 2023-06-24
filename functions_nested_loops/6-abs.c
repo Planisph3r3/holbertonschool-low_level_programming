aed33e437eb7b08e58fecc9b47926afc36dd3372
root <ennzopz@gmail.com>
2023-06-24 09:11:49 -0700

    Gives the absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - gives the absolute value of a number
+ *
+ *@a: value to be compared
+ *
+ * Return: a or -a value
+ */
+int _abs(int a)
+{
+if (a > 0)
+{
+return (a);
+}
+else
+{
+return (-a);
+}
+}
