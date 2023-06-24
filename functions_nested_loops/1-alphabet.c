afe539b9846d3c135bd93531d9e89f35ec46f87e
root <ennzopz@gmail.com>
2023-06-24 07:57:27 -0700

    Program that prints the whole alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
- * 
+ * print_alphabet - prints the whole alphabet
  *
- * Description: Program that prints the whole alphabet
- *
- * 
- */	
+ * Return: Void
+ */
 void print_alphabet(void)
 {
 char abc;
-
 for (abc = 'a'; abc <= 'z'; abc++)
 {
 _putchar(abc);
