13cf98890452b38958036762b5180f9f9f49d405
root <ennzopz@gmail.com>
2023-06-25 15:59:59 -0700

    Fizz-Buzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints from n#1-100 with a catch
+ *
+ * Description: multiples of three print Fizz &
+ * for the multiples of five print Buzz. If both
+ * prints FizzBuzz
+ *
+ * Return: Void
+ */
+void main(void)
+{
+	int a;
+for (a = 1; a <= 100; a++)
+{
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (a % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else if (a % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else
+	{
+		printf("%d", a);
+	}
+	if (a != 100)
+	{
+		printf(" ");
+	}
+}
+printf("\n");
+}
