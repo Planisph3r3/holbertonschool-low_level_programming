a25264eaec029be224621c29c536a27ba79a523a
root <ennzopz@gmail.com>
2023-06-26 16:31:43 -0700

    Header

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value n to 98
+ *
+ *@n: value to be changed
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef main_h
+#define main_h
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
