27059c95f1300e3d345b84ca3781d716c1a1355f
root <ennzopz@gmail.com>
2023-06-27 14:36:17 -0700

    Prints backwards a string using recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _print_rev_recursion - prints backwards a string using recursion
+ *
+ *@s: store the string
+ *
+ * Return: Void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
