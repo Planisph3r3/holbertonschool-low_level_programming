25fbc714991b2953d24ae5f6c76da2b96b0c157e
root <ennzopz@gmail.com>
2023-06-26 17:27:08 -0700

    Checks for how many characters a string have

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _strlen - checks for how many characters a string have
+ *
+ *@s: pointer of str value
+ *
+ * Return: 'a' value
+ */
+
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+
+}
+return (a);
+}
