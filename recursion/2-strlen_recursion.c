f15b7f853a5b6a3e015844bf3c74b69a81c90629
root <ennzopz@gmail.com>
2023-06-27 15:39:16 -0700

    Prints the length of a string recursively

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * _strlen_recursion - Prints the length of a string recursively
+ *
+ *@n: store the string
+ *
+ * Return: count
+ */
+int _strlen_recursion(char *n)
+{
+int count = 0;
+	if (*n == '\0')
+	{
+	return (0);
+	}
+	else
+	{
+	count++; 
+	count = count + _strlen_recursion(n + 1);
+	}
+	return (count);
+}
