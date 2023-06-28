868e8d483b268570cc0725dad1441ba49e963575
root <ennzopz@gmail.com>
2023-06-28 09:15:53 -0700

    Prints the name of the program itself

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - starts program
+ *
+ * Description: Prints the name of the program itself
+ *
+ * Return : Zero equals success
+ */
+
+int main (__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
