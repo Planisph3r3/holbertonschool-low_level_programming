c5792fcd2fde92eea11b72dcd91173b6fe3c8641
Planisph3r3 <ennzopz@gmail.com>
2023-08-27 18:45:32 -0700

    Returns a copy of the string given as a pointer

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _strdup - Returns a copy of the string given as a pointer
+ *
+ *@str: pointer to a string
+ *
+ * Return: A pointer to the duplicated string. NULL if failed
+ */
+
+
+
+char *_strdup(char *str)
+{
+	int j = 0;
+	int i = 0;
+	char *p;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+
+	p = malloc(i + 1);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	while (j <= 10)
+	{
+		p[j] = str[j];
+		j++;
+	}
+		return (p);
+}
