1847473b9a0b5c5a5695955790fd344b65443d5e
Planisph3r3 <ennzopz@gmail.com>
2023-09-03 15:57:10 -0700

    Freeing the dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * free_dog: a function that frees alloc memory from
  * the struct dog_t
- *
  *@d: pointer to dog_t variables.
  *
  */
@@ -12,7 +12,7 @@ void free_dog(dog_t *d)
 {
 if (d == NULL)
 {
-	return;
+	exit(0);
 }
 else
 {
