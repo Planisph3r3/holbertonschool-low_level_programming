88d9f0e6836ad8aad6faca2abf765fef20dc503e
Planisph3r3 <ennzopz@gmail.com>
2023-09-03 15:06:01 -0700

    Function that prints the struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+/**
+ * print_dog - function that prints the struct dog
+ *
+ *@d: pointer that point an initialized dog struct
+ *
+ * Return: Void.
+ */
+
+void print_dog(struct dog *d)
+{
+	char nil = nil;
+
+	if (d == NULL)
+	{
+		return;
+	}
+	if ((*d).name == NULL)
+	{
+	printf("Name: %s\n", nil);
+	else
+	{
+	printf("Name: %s\n", (*d).name);
+	}
+	printf("Age: %f\n". (*d).age);
+	if ((*d).owner == NULL)
+	{
+	printf("Owner: %s\n", nil);
+	}
+	else
+	{
+	printf("Owner: %s\n", (*d).owner);
+	}
+
