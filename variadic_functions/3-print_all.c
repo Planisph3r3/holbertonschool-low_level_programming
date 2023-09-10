e613dd061293784ea269b3eff6a8fa74096b41a3
Planisph3r3 <ennzopz@gmail.com>
2023-09-10 14:30:28 -0700

    A variadic function that prints diverse datatypes

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,44 +6,57 @@
  * @format: stores the type of data that is about to be printed  
  * Return: Void
  */
+#include <stdio.h>
+#include <stdarg.h>
+
 void print_all(const char * const format, ...)
-{
+{   
 const char *p = format;
 va_list datatype;
 va_start(datatype, format);
 
-while (*p)
-{const char actual_p = *p;
-switch (actual_p)
-{
-case 'c':
-{printf("%c", va_arg(datatype, int));
-break;
+    while (*p)
+    {
+        const char actual_p = *p;
+
+        switch (actual_p)
+        {
+            case 'c':
+                printf("%c", va_arg(datatype, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(datatype, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(datatype, double));
+                break;
+            case 's':
+            {
+                char *str = va_arg(datatype, char *);
+                int isNull = (str == NULL);
+
+                switch (isNull)
+                {
+                    case 1:
+                        printf("nil");
+                        break;
+                    default:
+                        printf("%s", str);
+                        break;
+                }
+                break;
+            }
+            default:
+                break;
+        }
+
+        p++;
+        if (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's')
+        {
+            printf(", ");
+        }
+    }
+
+    va_end(datatype);
+    printf("\n");
 }
-case 'i':
-{printf("%d", va_arg(datatype, int));
-break;
-}
-case 'f':
-{printf("%f", va_arg(datatype, double));
-break;
-}
-case 's':
-{char *str = (va_arg(datatype, char *));
-if (str == NULL)
-{printf("nil");
-}
-else
-{
-printf("%s", str);
-}
-break;
-}
-default:
-break; }
-p++;
-if (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's')
-{
-printf(", "); }}
-va_end(datatype);
-printf("\n"); }
